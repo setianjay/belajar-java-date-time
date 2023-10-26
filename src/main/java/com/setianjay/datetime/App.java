243cb032ccad7293caa911d2de8b86000132087b
Hari Setiaji <hari.setiaji2000@gmail.com>
2023-10-26 11:25:58 +0700

    Initial commit

---
+++ src/main/java/com/setianjay/datetime/App.java
@@ -0,0 +1,13 @@
+package com.setianjay.datetime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/setianjay/datetime/AppTest.java
@@ -0,0 +1,20 @@
+package com.setianjay.datetime;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
